c4669561a41f04cc443927c1d765086fc59bd50c
aneettabenny <abenny@siglarcarbon.com>
2022-05-13 16:18:43 +0530

    feat: add home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/containers/home/home.component';
 
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomeComponent },
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
