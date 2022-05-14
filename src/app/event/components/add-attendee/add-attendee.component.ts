490863bc4e8a1f2dc36da1c3583c64ff606a5c56
aneettabenny <abenny@siglarcarbon.com>
2022-05-14 18:53:02 +0530

    feat: create AddAttendeeComponent

---
+++ src/app/event/components/add-attendee/add-attendee.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { Attendee } from '../../../models';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-add-attendee',
+  templateUrl: './add-attendee.component.html',
+  styleUrls: ['./add-attendee.component.scss']
+})
+export class AddAttendeeComponent {
+  addAttendeeForm = new FormGroup({
+    name: new FormControl('', [Validators.required])
+  });
+
+  submit() {
+    const attendee = {
+      name: this.addAttendeeForm.value.name,
+      attending: true,
+      guests: 0
+    };
+    console.log('TCL: AddAttendeeComponent -> submit -> attendee', attendee);
+  }
+}
